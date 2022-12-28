d7636e8cd413a2e88a9888813de8aba822911d88
aravindh203 <aravindhkumar218@gmail.com>
2022-12-28 21:19:00 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var b=1;
+function hello(){
+  let a=new Date();
+  document.write(`The Time is  ${a.toLocaleTimeString()} <br><br>`);
+  if(b>10){
+    clearInterval(id);
+  }
+  b++;
+}
+let id=setInterval(hello,3000);
